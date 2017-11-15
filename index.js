0460f5c1e0aacd09162671f5d5a390f1b04921bd
Rupak Lamsal <rlamsal@wpi.edu>
2017-11-14 20:18:47 -0500

    finished auth flow

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/User");
+
+const User = mongoose.model("users");
+require("./services/passport");
+
+// Connect to the db
+mongoose.connect(keys.mongoURI);
+// mongoose.connect("mongodb://localhost:27017/userDb", function(err, db) {
+//   if (!err) {
+//     console.log("We are connected");
+//     User.findOne({ googleID: "1234" }).then(existingUser => {
+//       if (existingUser) {
+//         //we already have a record
+//         console.log("existing user");
+//       } else {
+//         // we dont have id, make a new record
+//         console.log("creating new user");
+//         new User({ googleID: "1234" }).save();
+//       }
+//     });
+//   }
+// });
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
